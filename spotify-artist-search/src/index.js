fd433df11204f9b13ca9eb4eac112d056e302d9e
David Folkins <david.folkins88@gmail.com>
2020-12-07 10:44:18 -0500

    Add search bar component

--- spotify-artist-search/src/index.js
+++ spotify-artist-search/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Landing from './components/Landing';
 import Navbar from './components/Navbar';
+import Searchbar from './components/Searchbar'
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
     <Landing />
+    <Searchbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
