8388dc1691ebeb4434a86bdfa35d27cec8827795
David Folkins <david.folkins88@gmail.com>
2020-12-08 13:49:52 -0500

    Add initial css styling to searchbar

--- spotify-artist-search/src/components/Searchbar.js
+++ spotify-artist-search/src/components/Searchbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import useDebounce from "../hooks/useDebounce";
+import searchIcon from "../images/Search_Icon.png"
 
 import './Searchbar.scss';
 
@@ -16,16 +17,17 @@ function Searchbar(props) {
 
 
   return (
-    <div className="Searchbar">
-      <form onSubmit={event => event.preventDefault()}>
-        <input
-          type="text"
-          name="search"
-          placeholder="Search for an artist..."
-          value={value}
-          onChange={event => setValue(event.target.value)}
-        />
-      </form>
+    <div className="searchBarContainer">
+        <form onSubmit={event => event.preventDefault()}>
+          <input className="searchBar"
+            type="text"
+            name="search"
+            placeholder="Search for an artist..."
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
+        </form>
+        <img className="searchIcon" src={searchIcon} />
     </div>
   );
 }
